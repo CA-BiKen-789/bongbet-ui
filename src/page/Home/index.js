d26026562632b6675745fddf33222263546ccd55
CA-BiKen-789 <cabiken789@gmail.com>
2023-08-11 15:08:42 +0700

    finished content layout

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,141 +1,38 @@
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
-import { useState } from "react";
+import Banner from "~/components/Banner";
 
 const cx = classNames.bind(styles);
 
 function Home() {
-    const [sizeImageIcon, setSizeImageIcon] = useState(null);
-    const [sizeImageSupport, setSizeImageSupport] = useState(null);
-
-    const listLink = [
-        {
-            urlImage: '/images/facebook_icon.png',
-            link: 'https://www.facebook.com/',
-            alt: 'Facebook icon'
-        },
-        {
-            urlImage: '/images/youtube_icon.png',
-            link: 'https://www.youtube.com/',
-            alt: 'Youtube icon'
-        },
-        {
-            urlImage: '/images/telegram_icon.png',
-            link: 'https://web.telegram.org/',
-            alt: 'Telegram icon'
-        },
-        {
-            urlImage: '/images/download_icon.png',
-            link: '#',
-            alt: 'Download icon'
-        },
-    ]
-
-    const listSupportLink = [
-        {
-            urlImage: '/images/ho_tro_nap_rut.png',
-            link: 'https://tawk.to/chat/649821bfcc26a871b0248efc/1h3p47l5r',
-            alt: 'Hỗ trợ nạp rút'
-        },
-        {
-            urlImage: '/images/ho_tro_khuyen_mai.png',
-            link: 'https://tawk.to/chat/6499407a94cf5d49dc5fd855/1h3ra7oq7',
-            alt: 'Hỗ trợ khuyến mãi'
-        },
-        {
-            urlImage: '/images/goi_tong_dai.png',
-            link: 'https://tawk.to/chat/649821bfcc26a871b0248efc/1h3p47l5r',
-            alt: 'Hỗ trợ tổng đài'
-        },
-        {
-            urlImage: '/images/ho_tro_facebook.png',
-            link: 'https://www.facebook.com/ThethaogiaitriBONGBET',
-            alt: 'Hỗ trợ facebook'
-        },
-        {
-            urlImage: '/images/ho_tro_khieu_nai.png',
-            link: 'https://t.me/khieunaibongbet',
-            alt: 'Hỗ trợ khiếu nại'
-        },
-    ]
 
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('banner')}>
-                <div className={cx('logo')}>
-                    <img src="/images/logo.png" alt="logo" />
-                </div>
-                <div className={cx('wrap-banner-content')}>
-                    <div className={cx('news-update')}>
-                        <img className={cx('title')} src="/images/banner_title.png" alt="banner_title" />
-                        <div className={cx('multi-platform')}>
-                            <img className={cx('promotion_news')} src="/images/promotion_news.png" alt="promotion_news"></img>
-                            <div className={cx('social-layout')}>
-                                <div className={cx('social')}>
-                                    {listLink.map((item, index) => (
-                                        <a key={index} href={item.link}
-                                            target="'_blank'">
-                                            <img
-                                                key={index}
-                                                src={item.urlImage}
-                                                alt={item.alt}
-                                                style={{
-                                                    transform: sizeImageIcon === index ? 'scale(1.02)' : 'scale(1)',
-                                                    transition: 'transform 0.1s ease-in-out'
-                                                }}
-                                                onMouseEnter={() => setSizeImageIcon(index)}
-                                                onMouseLeave={() => setSizeImageIcon(null)}
-                                            />
-                                        </a>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={cx('nav')}>
-                        {listSupportLink.map((item, index) => (
-                            <a key={index} href={item.link}
-                                target="'_blank'">
-                                <img
-                                    key={index}
-                                    src={item.urlImage}
-                                    alt={item.alt}
-                                    style={{
-                                        transform: sizeImageSupport === index ? 'scale(1.06)' : 'scale(1)',
-                                        transition: 'transform 0.2s ease-in-out'
-                                    }}
-                                    onMouseEnter={() => setSizeImageSupport(index)}
-                                    onMouseLeave={() => setSizeImageSupport(null)}
-                                />
-                            </a>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            <Banner />
             <div className={cx('content')}>
                 <div className={cx('wrapper-content')}>
                     <div className={cx('connect')}>
-                        <div className={cx('title')}>
+                        <div className={cx('connect-title')}>
                             <img src="/images/title_connect.png" alt="Các đường truyền tốc độ cao" />
                         </div>
                         <ul className={cx('connect-point')}>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Cần Thơ</a>
+                                <a href="/"><span>Cần Thơ</span></a>
                             </li>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Đà Nẵng</a>
+                                <a href="/"><span>Đà Nẵng</span></a>
                             </li>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Hồ Chí Minh</a>
+                                <a href="/"><span>Hồ Chí Minh</span></a>
                             </li>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Phú Quốc</a>
+                                <a href="/"><span>Phú Quốc</span></a>
                             </li>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Nha Trang</a>
+                                <a href="/"><span>Nha Trang</span></a>
                             </li>
                             <li className={cx('connect-point_item')}>
-                                <a href="/">Hà Nội</a>
+                                <a href="/"><span>Hà Nội</span></a>
                             </li>
                         </ul>
                     </div>
@@ -147,8 +44,8 @@ function Home() {
                             <h1>hỏi đáp về bongbet</h1>
                             <p>Dưới đây là một vài câu hỏi của người chơi được chúng tôi tổng hợp lại.
                                 Nếu có bất kỳ thắc mắc sẽ được giải đáp chi tiết nhất.</p>
+                            <a href="/">xem chi tiết</a>
                         </div>
-                        <button>xem chi tiết</button>
                         <div className={cx('conclusion')}>
                             <h1>KẾT LUẬN</h1>
                             <p>Những điều bạn cần hiểu và biết về nhà cái <span>BONGBET</span> đã được đề cập khá cụ thể.
